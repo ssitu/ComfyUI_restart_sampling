5872914f141f29910defa68f2a60274b39a913cd
ssit <ssit5568@gmail.com>
2023-11-02 22:23:20 -0400

    ddim has been redirected to ksampler euler
    Fix for comfyui commits 1777b54 and a268a57

--- restart_schedulers.py
+++ restart_schedulers.py
@@ -1,5 +1,6 @@
 import torch
 from comfy.k_diffusion import sampling as k_diffusion_sampling
+# from comfy.samplers import normal_scheduler
 
 
 def get_sigmas_karras(model, n, s_min, s_max, device):
@@ -10,10 +11,29 @@ def get_sigmas_exponential(model, n, s_min, s_max, device):
     return k_diffusion_sampling.get_sigmas_exponential(n, s_min, s_max, device=device)
 
 
+def normal_scheduler(model, steps, s_min, s_max, sgm=False, floor=False):
+    """
+    Pulled from comfy.samplers.normal_scheduler
+    """
+    s = model.model_sampling
+    start = s.timestep(torch.tensor(s_max))
+    end = s.timestep(torch.tensor(s_min))
+
+    if sgm:
+        timesteps = torch.linspace(start, end, steps + 1)[:-1]
+    else:
+        timesteps = torch.linspace(start, end, steps)
+
+    sigs = []
+    for x in range(len(timesteps)):
+        ts = timesteps[x]
+        sigs.append(s.sigma(ts))
+    sigs += [0.0]
+    return torch.FloatTensor(sigs)
+
+
 def get_sigmas_normal(model, n, s_min, s_max, device):
-    t_min, t_max = model.sigma_to_t(torch.tensor([s_min, s_max], device=device))
-    t = torch.linspace(t_max, t_min, n, device=device)
-    return k_diffusion_sampling.append_zero(model.t_to_sigma(t))
+    return normal_scheduler(model.inner_model.inner_model, n, s_min, s_max).to(device)
 
 
 def get_sigmas_simple(model, n, s_min, s_max, device):

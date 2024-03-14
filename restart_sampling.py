6ddbaf02f4ce1459900b41eaccd3ae8c38a0bcea
blepping <blepping@tuta.io>
2024-03-14 05:41:27 -0600

    Add KRestartSamplerCustom + other cleanups

--- restart_sampling.py
+++ restart_sampling.py
@@ -2,8 +2,10 @@ import ast
 import warnings
 import torch
 from tqdm.auto import trange
-from nodes import common_ksampler
-from comfy.k_diffusion import sampling as k_diffusion_sampling
+import latent_preview
+import comfy
+from comfy.sample import sample_custom, prepare_noise
+from comfy.samplers import KSAMPLER, sampler_object
 from comfy.utils import ProgressBar
 from .restart_schedulers import SCHEDULER_MAPPING
 
@@ -67,80 +69,126 @@ def calc_restart_steps(restart_segments):
     return restart_steps
 
 
-_total_steps = 0
-_restart_segments = None
-_restart_scheduler = None
+def restart_sampling(model, seed, steps, cfg, sampler, scheduler, positive, negative, latent_image, restart_info, restart_scheduler, denoise=1.0, disable_noise=False, start_step=None, last_step=None, force_full_denoise=False, output_only=True):
+    total_steps = [0]
+    restart_segments = prepare_restart_segments(restart_info)
 
-
-def restart_sampling(model, seed, steps, cfg, sampler_name, scheduler, positive, negative, latent_image, restart_info, restart_scheduler, denoise=1.0, disable_noise=False, start_step=None, last_step=None, force_full_denoise=False):
-    global _total_steps, _restart_segments, _restart_scheduler
-    _restart_scheduler = restart_scheduler
-    _restart_segments = prepare_restart_segments(restart_info)
-
-    if sampler_name == "ddim":
-        # ddim is redirected to euler
-        sampler_wrapper = KSamplerRestartWrapper("euler")
-    else:
-        sampler_wrapper = KSamplerRestartWrapper(sampler_name)
+    if isinstance(sampler, str):
+        sampler = sampler_object(sampler)
 
     # Add the additional steps to the progress bar
     pbar_update_absolute = ProgressBar.update_absolute
 
     def pbar_update_absolute_wrapper(self, value, total=None, preview=None):
-        pbar_update_absolute(self, value, _total_steps, preview)
+        pbar_update_absolute(self, value, total_steps[0], preview)
 
     ProgressBar.update_absolute = pbar_update_absolute_wrapper
 
+    comfy.model_management.load_models_gpu([model])
+    real_model = model
+    while hasattr(real_model, "model"):
+        real_model = real_model.model
+    sigmas = calc_sigmas(scheduler, steps,
+        float(real_model.model_sampling.sigma_min), float(real_model.model_sampling.sigma_max),
+        real_model, model.load_device,
+    )
+
+    if last_step < (len(sigmas) - 1):
+        sigmas = sigmas[:last_step + 1]
+        if force_full_denoise:
+            sigmas[-1] = 0
+
+    if start_step < (len(sigmas) - 1):
+        sigmas = sigmas[start_step:]
+
+    sampler_wrapper = KSamplerRestartWrapper(sampler, real_model, restart_scheduler, restart_segments, total_steps)
+
+    latent = latent_image
+    latent_image = latent["samples"]
+    if disable_noise:
+        torch.manual_seed(seed) # workaround for https://github.com/comfyanonymous/ComfyUI/issues/2833
+        noise = torch.zeros(latent_image.size(), dtype=latent_image.dtype, layout=latent_image.layout, device="cpu")
+    else:
+        batch_inds = latent["batch_index"] if "batch_index" in latent else None
+        noise = prepare_noise(latent_image, seed, batch_inds)
+
+    noise_mask = None
+    if "noise_mask" in latent:
+        noise_mask = latent["noise_mask"]
+
+    x0_output = {}
+    callback = latent_preview.prepare_callback(model, sigmas.shape[-1] - 1, x0_output)
+
+    disable_pbar = not comfy.utils.PROGRESS_BAR_ENABLED
+
+    sampler = KSAMPLER(
+        sampler_wrapper.ksampler_restart_wrapper, extra_options=sampler.extra_options | {},
+        inpaint_options=sampler.inpaint_options | {},
+    )
+
     try:
-        samples = common_ksampler(model, seed, steps, cfg, sampler_name, scheduler, positive, negative, latent_image, denoise=denoise,
-                                  disable_noise=disable_noise, start_step=start_step, last_step=last_step, force_full_denoise=force_full_denoise)
+        samples = sample_custom(
+            model, noise, cfg, sampler, sigmas, positive, negative, latent_image,
+            noise_mask=noise_mask, callback=callback, disable_pbar=disable_pbar, seed=seed)
     finally:
-        sampler_wrapper.cleanup()
         ProgressBar.update_absolute = pbar_update_absolute
-    return samples
+
+    out = latent.copy()
+    out["samples"] = samples
+
+    if output_only:
+        return (out,)
+
+    if "x0" in x0_output:
+        out_denoised = latent.copy()
+        out_denoised["samples"] = model.model.process_latent_out(x0_output["x0"].cpu())
+    else:
+        out_denoised = out
+
+    return (out, out_denoised)
 
 
 class KSamplerRestartWrapper:
 
     ksampler = None
 
-    def __init__(self, sampler_name):
-        self.sample_func_name = "sample_{}".format(sampler_name)
-        KSamplerRestartWrapper.ksampler = getattr(k_diffusion_sampling, self.sample_func_name)
-        setattr(k_diffusion_sampling, self.sample_func_name, self.ksampler_restart_wrapper)
+    def __init__(self, sampler, real_model, restart_scheduler, restart_segments, total_steps):
+        self.ksampler = sampler
+        self.real_model = real_model
+        self.restart_scheduler = restart_scheduler
+        self.restart_segments = restart_segments
+        self.total_steps = total_steps
 
-    def cleanup(self):
-        setattr(k_diffusion_sampling, self.sample_func_name, KSamplerRestartWrapper.ksampler)
-
-    @staticmethod
     @torch.no_grad()
-    def ksampler_restart_wrapper(model, x, sigmas, extra_args=None, callback=None, disable=None):
-        global _total_steps, _restart_segments, _restart_scheduler
-        ksampler = KSamplerRestartWrapper.ksampler
-        segments = round_restart_segments(sigmas, _restart_segments)
-        _total_steps = len(sigmas) - 1 + calc_restart_steps(segments)
+    def ksampler_restart_wrapper(self, model, x, sigmas, *args, extra_args=None, callback=None, disable=None, **kwargs):
+        ksampler = self.ksampler
+        segments = round_restart_segments(sigmas, self.restart_segments)
+        self.total_steps[0] = len(sigmas) - 1 + calc_restart_steps(segments)
         step = 0
 
         def callback_wrapper(x):
             x["i"] = step
             if callback is not None:
                 callback(x)
-        with trange(_total_steps, disable=disable) as pbar:
+        with trange(self.total_steps[0], disable=disable) as pbar:
             for i in range(len(sigmas) - 1):
-                x = ksampler(model, x, torch.tensor([sigmas[i], sigmas[i + 1]],
-                                                    device=x.device), extra_args, callback_wrapper, True)
+                x = ksampler.sampler_function(
+                    model, x, torch.tensor([sigmas[i], sigmas[i + 1]],
+                    device=x.device), *args, extra_args=extra_args, callback=callback_wrapper, disable=True,
+                    **kwargs)
                 pbar.update(1)
                 step += 1
                 if sigmas[i + 1].item() in segments:
                     seg = segments[sigmas[i + 1].item()]
                     s_min, s_max, k, n_restart = sigmas[i + 1], seg['t_max'], seg['k'], seg['n']
-                    seg_sigmas = calc_sigmas(_restart_scheduler, n_restart, s_min,
-                                             s_max, model, device=x.device)
+                    seg_sigmas = calc_sigmas(self.restart_scheduler, n_restart, s_min,
+                                             s_max, self.real_model, device=x.device)
                     for _ in range(k):
                         x += torch.randn_like(x) * (s_max ** 2 - s_min ** 2) ** 0.5
                         for j in range(n_restart - 1):
-                            x = ksampler(model, x, torch.tensor(
-                                [seg_sigmas[j], seg_sigmas[j + 1]], device=x.device), extra_args, callback_wrapper, True)
+                            x = ksampler.sampler_function(model, x, torch.tensor(
+                                [seg_sigmas[j], seg_sigmas[j + 1]], device=x.device), *args, extra_args=extra_args,
+                                callback=callback_wrapper, disable=True, **kwargs)
                             pbar.update(1)
                             step += 1
         return x
